6a1ee9d1ec2bca5ae3a4ff8697fbe44eb9f7d01e
thanhtunguet <thanhtung.uet@gmail.com>
2026-04-13 18:36:02 +0700

    chore: ignore js files

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 
 export default defineConfig([
-  globalIgnores(['assets/*', 'node_modules/*', '_site/*']),
+  globalIgnores(['assets/*', 'node_modules/*', '_site/*', '_app/*']),
   js.configs.recommended,
   {
     rules: {
